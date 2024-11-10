7be601e21ef99be61cbc798a332f6797c99161fc
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-10 13:43:04 +0100

    📈 fetch  users from db & display in the front 📈

--- app/components/ListUsers.tsx
+++ app/components/ListUsers.tsx
@@ -1,11 +1,34 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderListUsers from './HeaderListUsers'
 import TableUsers from './TableUsers'
 import { UserData } from '../utils/Types';
 
 const ListUsers = () => {
   const [users, setUsers] = useState<UserData[]>([]);
+
+
+   // Fonction pour récupérer les utilisateurs depuis le backend
+   useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`);
+        if (response.ok) {
+          const data = await response.json();
+          setUsers(data);
+        } else {
+          console.error("Erreur lors de la récupération des utilisateurs:", response.status);
+        }
+      } catch (error) {
+        console.error("Erreur de réseau :", error);
+      }
+    };
+
+    fetchUsers(); // Appelle la fonction pour récupérer les utilisateurs
+  }, []);
+
+
+  
 
   const handleAddUser = (newUser: UserData) => {
     setUsers((prevUsers) => [...prevUsers, newUser]);
