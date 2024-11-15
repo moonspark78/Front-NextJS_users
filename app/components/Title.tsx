37f15cc601fb0774c286c72fc573e340ba4d4c6b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-15 15:21:31 +0100

    📈 create button logout  📈

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -3,8 +3,13 @@ import React from "react";
 const Title = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="flex justify-center items-center border border-gray-300 p-4 rounded-md w-4/5 mt-3">
+      <div className="flex justify-between items-center border border-gray-300 p-4 rounded-md w-4/5 mt-3">
         <h1 className="lg:text-4xl text-2xl underline">Dashboard</h1>
+        <button>
+          <span className="underline cursor-pointer text-md">
+            Se déconnecter
+          </span>
+        </button>
       </div>
     </div>
   );
