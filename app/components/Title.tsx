ae18d241d8b13bfc745ff407768a10d698b0b31f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-15 15:48:43 +0100

    📈 done  logout  📈

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,11 +1,23 @@
+'use client';
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Title = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Supprimer le token du localStorage
+    localStorage.removeItem("authToken");
+    // Utiliser le router de Next.js pour la redirection
+    router.push("/login");
+  };
+
   return (
     <div className="flex items-center justify-center">
       <div className="flex justify-between items-center border border-gray-300 p-4 rounded-md w-4/5 mt-3">
         <h1 className="lg:text-4xl text-2xl underline">Dashboard</h1>
-        <button>
+        <button onClick={handleLogout}>
           <span className="underline cursor-pointer text-md">
             Se déconnecter
           </span>
@@ -16,3 +28,4 @@ const Title = () => {
 };
 
 export default Title;
+
